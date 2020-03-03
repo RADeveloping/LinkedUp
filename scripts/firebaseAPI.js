4e3e9379907aa82501a194d2a46a2d3f966e8f9d
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2020-03-03 14:16:17 -0800

    created the base files for the project. All the external JS files are linked into one and imported. Please dont edit the externalJS file or the firebaseAPI js fileeveryones, js must go into script.js file! PLEASE CREATE SEPERATE FUNCTIONS AND CALL THOSE FUNCTIONS...

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,17 @@
+ // DO NOT MODIFY THIS FILE //
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyAzerA-zdbvh-Thp6UYFn5qKbYc2_za72A",
+    authDomain: "techmini-e3a2a.firebaseapp.com",
+    databaseURL: "https://techmini-e3a2a.firebaseio.com",
+    projectId: "techmini-e3a2a",
+    storageBucket: "techmini-e3a2a.appspot.com",
+    messagingSenderId: "281432190044",
+    appId: "1:281432190044:web:ccdc6f459fdb16cbdcca99"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// DO NOT MODIFY THIS FILE //
