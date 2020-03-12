ea3512445531a2901a29da797e10a183f6a81c2f
Rahim Askarzadeh <raskarzadeh1@my.bcit.ca>
2020-03-11 20:16:39 -0700

    updated login page

--- scripts/login.js
+++ scripts/login.js
@@ -17,6 +17,23 @@
 // JS FOR LOGIN.HTML
 //--------------------------------------------------------------------
 
+function init(){
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+         //   window.location.assign("main.html");
+        } else {
+          // No user is signed in.
+        }
+      });
+
+    
+}
+
+  
+
+function loadFirebaseUI() {
+    
        // This login snippet goes at the end of the body tag
         // After firebase libraries via CDN are sourced
         // After your firebase project API config is defined
@@ -28,64 +45,74 @@
         // - create rules to allow for read/write
         // - enable authentication method (email/pwd signin)
         
-        // Initialize the FirebaseUI Widget using Firebase.
-        var ui = new firebaseui.auth.AuthUI(firebase.auth());
-        var uiConfig = {
-            callbacks: {
-                signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-                    // User successfully signed in.
-                    // Return type determines whether we continue the redirect automatically
-                    // or whether we leave that to developer to handle.
-                    //------------------------------------------------------------------------------------------
-                    // The code below is modified from default snippet provided by the documentation
-                    //
-                    // If the user is a "brand new" user, then create a new "user" in your own database
-                    // Assign this user with the name and email provided.
-                    // Before this works, you must enable "Firestore" from the firebase console.
-                    // The Firestore rules must allow the user to write. 
-                    //------------------------------------------------------------------------------------------
-                    
-                    var user = authResult.user;
-                    if (authResult.additionalUserInfo.isNewUser) {
-                        db.collection("users").doc(user.uid).set({
-                                name: user.displayName,
-                                email: user.email
-                            }).then(function () {
-                                console.log("New user added to firestore");
-                                window.location.assign("main.html");
-                            })
-                            .catch(function (error) {
-                                console.log("Error adding new user: " + error);
-                            });
-                    } else {
-                        return true;
-                    }
-                    return false;
-                },
-                uiShown: function () {
-                    // The widget is rendered.
-                    // Hide the loader.
-                    document.getElementById('loader').style.display = 'none';
+     
+   // Initialize the FirebaseUI Widget using Firebase.
+   
+   var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+                // User successfully signed in.
+                // Return type determines whether we continue the redirect automatically
+                // or whether we leave that to developer to handle.
+                //------------------------------------------------------------------------------------------
+                // The code below is modified from default snippet provided by the documentation
+                //
+                // If the user is a "brand new" user, then create a new "user" in your own database
+                // Assign this user with the name and email provided.
+                // Before this works, you must enable "Firestore" from the firebase console.
+                // The Firestore rules must allow the user to write. 
+                //------------------------------------------------------------------------------------------
+                var user = authResult.user;
+                if (authResult.additionalUserInfo.isNewUser) {
+                    db.collection("users").doc(user.uid).set({
+                        name: user.displayName,
+                        email: user.email
+                    }).then(function () {
+                        console.log("New user added to firestore");
+                        window.location.assign("main.html");
+                    })
+                        .catch(function (error) {
+                            console.log("Error adding new user: " + error);
+                        });
                 }
+                else {
+                    return true;
+                }
+                return false;
             },
-            // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-            signInFlow: 'popup',
-            signInSuccessUrl: 'main.html',
-            signInOptions: [
-                // Leave the lines as is for the providers you want to offer your users.
-                //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                //firebase.auth.PhoneAuthProvider.PROVIDER_ID
-            ],
-            // Terms of service url.
-            tosUrl: 'main.html',
-            // Privacy policy url.
-            privacyPolicyUrl: 'main.html',
-            accountChooserEnabled: false
-        };
-        // The start method will wait until the DOM is loaded.
-        // Inject the login interface into the HTML
-        ui.start('#firebaseui-auth-container', uiConfig);
+            uiShown: function () {
+                // The widget is rendered.
+                // Hide the loader.
+                document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: 'main.html',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        // Terms of service url.
+        tosUrl: 'main.html',
+        // Privacy policy url.
+        privacyPolicyUrl: 'main.html',
+        accountChooserEnabled: false
+    };
+    // The start method will wait until the DOM is loaded.
+    // Inject the login interface into the HTML
+    ui.start('#firebaseui-auth-container', uiConfig);
+}
+
+function validateEmail(){
+    console.log("output" + document.getElementById("inputEmail"));
+}
+
+
+init();
+
