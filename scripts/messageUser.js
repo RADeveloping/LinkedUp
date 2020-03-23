4a384423e8c84098c7f52d1e0d83a5c43d52c9a5
William He <contact@williamhe.dev>
2020-03-22 23:15:32 -0700

    Re-imported Firebase and fixed loading order.

--- scripts/messageUser.js
+++ scripts/messageUser.js
@@ -1,23 +1,6 @@
-/*
-var firebaseConfig = {
-    apiKey: "AIzaSyAzerA-zdbvh-Thp6UYFn5qKbYc2_za72A",
-    authDomain: "techmini-e3a2a.firebaseapp.com",
-    databaseURL: "https://techmini-e3a2a.firebaseio.com",
-    projectId: "techmini-e3a2a",
-    storageBucket: "techmini-e3a2a.appspot.com",
-    messagingSenderId: "281432190044",
-    appId: "1:281432190044:web:ccdc6f459fdb16cbdcca99"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-*/
-
-//-----------------------------------------------------------------------------
-/*
 let userId = firebase.UserInfo.uid;
 let userCollection = db.collection("users").doc(userId);
-*/
+
 const month = 
     ["January", "February", 
     "March", "April", 
