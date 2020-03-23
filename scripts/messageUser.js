a0413e6c624698049b53aecb0de12e472b2ea247
William He <contact@williamhe.dev>
2020-03-22 17:27:05 -0700

    Updated js functions

--- scripts/messageUser.js
+++ scripts/messageUser.js
@@ -1,3 +1,4 @@
+/*
 var firebaseConfig = {
     apiKey: "AIzaSyAzerA-zdbvh-Thp6UYFn5qKbYc2_za72A",
     authDomain: "techmini-e3a2a.firebaseapp.com",
@@ -10,36 +11,58 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+*/
 
 //-----------------------------------------------------------------------------
-
+/*
 let userId = firebase.UserInfo.uid;
 let userCollection = db.collection("users").doc(userId);
+*/
+const month = 
+    ["January", "February", 
+    "March", "April", 
+    "May", "June", 
+    "July", "August", 
+    "September", "October", 
+    "November", "December"];
 
 function sendMessage() {
-    let message = document.getElementById("btn-chat").value;
+    let message = document.getElementById("btn-input").value;
 
-    createMessageDiv();
+    createMessageDiv(message);
 }
 
-function createMessageDiv() {
-    let allMessages = document.getElementById("allMessage");
+function createMessageDiv(message) {
+    let allMessages = document.getElementById("allMessages");
+    let today = new Date();
 
     let messageDiv = document.createElement("div");
     messageDiv.setAttribute("class", "messageDiv");
     
     let profilePic = document.createElement("img");
     profilePic.setAttribute("class", "profilePic");
+    //profilePic.src = "https://via.placeholder.com/100.png/f00";
 
     let userName = document.createElement("p");
     userName.setAttribute("class", "userName");
+    //userName.innerHTML = "Bob";
 
     let userMessage = document.createElement("p");
     userMessage.setAttribute("class", "userMessage");
+    //userMessage.innerHTML = message;
 
     let sentDate = document.createElement("p");
     sentDate.setAttribute("class", "sentDate");
+    sentDate.innerHTML = 
+        month[today.getMonth()] + " " 
+        + today.getDate() + ", " 
+        + today.getFullYear() + ", " 
+        + today.getHours() + ":" 
+        + today.getMinutes();
 
-    messageDiv.appendChild(profilePic, userName, userMessage, sentDate);
+    messageDiv.appendChild(profilePic);
+    messageDiv.appendChild(userName);
+    messageDiv.appendChild(userMessage);
+    messageDiv.appendChild(sentDate);
     allMessages.appendChild(messageDiv);
 }
